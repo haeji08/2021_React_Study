ddc4fa2870bcdca12cd7eb6778c6ccb2d67aa29a
haeji08 <83302507+haeji08@users.noreply.github.com>
2021-11-10 04:51:29 +0900

    Add files via upload
    ReduxWeather

---
+++ src/components/google_map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+export default (props) =>  {
+  console.log(props);
+  return (
+    <div style={{height:'150px', width:'200px'}}>
+      <GoogleMapReact
+        defaultZoom={12}
+        defaultCenter={{lat : props.lat, lng:props.lon}} />
+    </div>
+  );
+}
